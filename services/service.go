5d8be4844cea432d610f708be02dc80d48a7ccd5
Dinesh <dinesh240894@gmail.com>
2025-04-26 01:59:56 +0400

    add parent child rel in services

--- services/service.go
+++ services/service.go
@@ -18,6 +18,25 @@ type Service struct {
 	URL         string    `yaml:"url"`
 	SubServices []Service `yaml:"sub_services"`
 	LogoPath    string    `yaml:"logo_path"`
+
+	// Parent points to the parent Service in the hierarchy.
+	// It is set after unmarshalling to allow navigating upward in the service tree.
+	Parent *Service `yaml:"-"`
+}
+
+func (s *Service) Title() string {
+	if s.IsChild() {
+		return s.Parent.ID + " " + s.ID
+	}
+	return s.ID
+}
+
+func (s *Service) Autocomplete() string {
+	if s.IsChild() {
+		return s.Parent.ID + " " + s.ID
+	}
+
+	return s.ID
 }
 
 func (s *Service) Icon() *aw.Icon {
@@ -40,11 +59,24 @@ func (s *Service) Url(config *gcloud.Config) (string, error) {
 }
 
 func (s *Service) Subtitle() string {
-	if len(s.SubServices) > 0 {
-		return "🗂️⬇️ " + s.Description
+	if s.IsParent() {
+		suffix := s.Name + " - " + s.Description
+		if len(s.SubServices) > 0 {
+			return "🗂️ " + suffix
+		}
+		return suffix
+	} else if s.IsChild() {
+		return s.Parent.Name + " - " + s.Name
 	}
-
 	return s.Description
+}
+
+func (s *Service) IsParent() bool {
+	return s.Parent == nil
+}
+
+func (s *Service) IsChild() bool {
+	return s.Parent != nil
 }
 
 // Load reads the services from an embedded YAML file
@@ -59,5 +91,17 @@ func Load(file embed.FS) ([]Service, error) {
 		return nil, err
 	}
 
+	linkParentChild(services)
 	return services, nil
+}
+
+func linkParentChild(services []Service) {
+	for i := range services {
+		parent := &services[i]
+		for j := range parent.SubServices {
+			child := &parent.SubServices[j]
+			child.Parent = parent
+			linkParentChild(child.SubServices)
+		}
+	}
 }
