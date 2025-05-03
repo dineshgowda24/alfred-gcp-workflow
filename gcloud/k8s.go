da0a09c0fb5921b499ef987af8e4f0ac89bdd951
Dinesh <dinesh240894@gmail.com>
2025-05-04 01:02:31 +0400

    add region support to filestore and k8s

--- gcloud/k8s.go
+++ gcloud/k8s.go
@@ -1,20 +1,22 @@
 package gcloud
 
 type K8sCluster struct {
-	Name string `json:"name"`
-	// one of [STATUS_UNSPECIFIED PROVISIONING RUNNING RECONCILING STOPPING ERROR DEGRADED]
-	Status string `json:"status"`
-	// Format 2023-11-01T09:09:29+00:00
-	CreatedAt            string `json:"createTime"`
+	Name                 string `json:"name"`
+	Status               string `json:"status"`
 	Location             string `json:"location"`
 	CurrentMasterVersion string `json:"currentMasterVersion"`
 	CurrentNodeCount     int    `json:"currentNodeCount"`
+	CreatedAt            string `json:"createTime"`
+}
+
+func (c K8sCluster) IsRegionSupported(config *Config) bool {
+	return isComputeRegionSupported(config)
 }
 
 func ListK8sClusters(config *Config) ([]K8sCluster, error) {
-	return runGCloudCmd[[]K8sCluster](
-		config,
-		"container", "clusters", "list",
-		"--format=json(name,location,status,createTime,currentNodeCount,currentMasterVersion)",
-	)
+	args := []string{"clusters", "list", "--format=json(name,status,location,currentMasterVersion,currentNodeCount,createTime)"}
+	if config != nil && config.GetRegionName() != "" {
+		args = append(args, "--region="+config.GetRegionName())
+	}
+	return runGCloudCmd[[]K8sCluster](config, "container", args...)
 }
