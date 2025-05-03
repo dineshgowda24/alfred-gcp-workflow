b48cffeddaf19c670f685ec540b406c9cbfdc5f9
Dinesh <dinesh240894@gmail.com>
2025-05-04 03:30:55 +0400

    add support for regions in missin gcloud cmd

--- gcloud/storage.go
+++ gcloud/storage.go
@@ -1,13 +1,13 @@
 package gcloud
 
 type Bucket struct {
-	CreationTime        string `json:"creation_time"` // Format 2020-09-28T09:24:57+0000 unparsable to time.Time
+	CreationTime        string `json:"creation_time"`
 	DefaultStorageClass string `json:"default_storage_class"`
 	Location            string `json:"location"`
 	LocationType        string `json:"location_type"`
 	Name                string `json:"name"`
 	StorageURL          string `json:"storage_url"`
-	UpdateTime          string `json:"update_time"` // Format 2020-09-28T09:24:57+0000 unparsable to time.Time
+	UpdateTime          string `json:"update_time"`
 }
 
 func ListCloudStorageBuckets(config *Config) ([]Bucket, error) {
